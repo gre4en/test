a9a2fc615f486087fba053074ea86804361575c0
gre4en <gre4enit@gmail.com>
2017-05-23 16:58:47 +0300

    Initial Commit

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-IP=$(netstat -r | grep 'default' | cut -d: -f2 | awk '{ print $2}') 
-echo "$IP"
+
+sudo cp -f /etc/hosts tmp.txt
+sudo rm -f /etc/hosts
+sudo touch /etc/hosts
+while read line; do
+  [ "${line:0:9}" != "127.0.0.1" ] && [ "${line:0:3}" != "::1" ] && sudo sh -c "echo '$line' >> /etc/hosts"
+done < tmp.txt
+sudo sh -c "echo '127.0.0.1 localhost' >> /etc/hosts"
+sudo sh -c "echo '::1 ip6-localhost ip6-loopback localhost' >> /etc/hosts"
+
